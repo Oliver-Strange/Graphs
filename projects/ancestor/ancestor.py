a97f635e55abdf2c1dfd0a75dd1afdd9ee41d2d6
Oliver-Strange <preston.burton@gmail.com>
2019-11-05 15:27:53 -0800

    adds queue and graph classes to ancestor, adds some notes/pseudo code

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,43 @@
+# add queue stuff?
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+# add graph stuff
+
+
+class Graph():
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("can not make edge")
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
+    # passing pairs of vertices (parent, child)
+    # add edges to link them
+    # probably do a BFS to find children
+    # no ancestor = -1
+    # ties = lowest numeric ID
+    #
